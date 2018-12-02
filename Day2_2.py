b0570cbb199a637ff0e4a3db526f81451497637c
Paul Minda <jpminda@uwo.ca>
2018-12-02 09:45:03 -0500

    Update Day2_2.py

--- Day2_2.py
+++ Day2_2.py
@@ -6,6 +6,13 @@ with open('day2.txt') as f:
 size = len(d)
 match = 0
 
+#loop though all the exemplars
+#for each exeamplars i comparare to all the exemaplrs except itself
+#if theere is a mismatch, count how many
+#then if you get a count of 1, record those two values.
+#then remove any non matching letters
+#that is the answer 
+
 for i in range(size):
 	match = 0
 	dd = list(d[i])
