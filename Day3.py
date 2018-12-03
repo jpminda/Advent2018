29ffdfca0d1613b61db8538807aed622e17a9342
Paul Minda <jpminda@uwo.ca>
2018-12-03 15:19:51 -0500

    Update Day3.py

--- Day3.py
+++ Day3.py
@@ -1,18 +1,24 @@
 import numpy as np
 import re
-size = 2000
-#import matplotlib.pyplot as plt
+size = 1000
+import matplotlib.pyplot as plt
 claims = np.zeros(shape=(size,size))
-print(claims)
+tempclaims = np.zeros(shape=(size,size))
 f = open('day3input.txt')
 for line in f:
 	ID,startX,startY,sizeX,sizeY = map(int, re.findall(r'\d+', line))
-	claims[startX:startX + sizeX, startY:startY + sizeY] = claims[startX:startX + sizeX, startY:startY + sizeY]+1
-print(ID)
-print(startX)
-print(startY)
-print(sizeX)
-print(sizeY)
+	tempclaims[startX:startX + sizeX, startY:startY + sizeY] = 1
+	if tempclaims.any()!=claims.any():
+		print(ID)
+	claims[startX:startX + sizeX, startY:startY + sizeY] = claims[startX:startX + sizeX, startY:startY + sizeY]+ tempclaims[startX:startX + sizeX, startY:startY + sizeY]
+	if tempclaims.any()!=claims.all():
+		print(ID)
 print(claims)
 final = sum(sum(claims>=2))
 print(final)
+plt.imshow(claims)
+plt.show()
+
+
+
+
