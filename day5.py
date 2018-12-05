54c185bb95a17d81604fe338811c2786fda4903d
Paul Minda <jpminda@uwo.ca>
2018-12-05 09:47:43 -0500

    day 5 final

--- day5.py
+++ day5.py
@@ -1,23 +1,57 @@
 #test example
-#polymer = str('dabAcCaCBAcCcaDA')
+#filename = ('day5inputE.txt')
+
 filename = ('day5input.txt')
 with open(filename, 'r') as f:
-    polymer = f.read()
+    orig_polymer = f.read()
+
+new_polymer = orig_polymer
 
 matches = True
+
 
 while matches == True:	
 	matches = False
 	i = 0
 	#print(polymer[i],polymer[i+1])
 	#if polymer[i].islower() ==  polymer[i+1].isupper():
-	while i < (len(polymer) - 1):
-		if polymer[i] ==  polymer[i + 1].swapcase():
+	while i < (len(new_polymer) - 1):
+		if new_polymer[i] ==  new_polymer[i + 1].swapcase():
 			matches = True
-			new_polymer = polymer.replace(polymer[i] + polymer[i + 1], '', 1)
-			polymer = new_polymer
+			new_polymer = new_polymer.replace(new_polymer[i] + new_polymer[i + 1], '', 1)
+			#polymer = new_polymer
 		else:	
 			i = i + 1
 
-print(polymer)
-print(len(polymer))
+#correct answer for part 1
+print(len(new_polymer)) 
+
+#part 2
+polymer_size = []
+for i in range(25):
+	new_polymer = orig_polymer
+	s = chr(i + 65)
+	new_polymer = new_polymer.replace(s, '')
+	S = chr(i + 97)
+	new_polymer = new_polymer.replace(S, '')
+	matches = True
+	while matches == True:	
+		matches = False
+		i = 0
+		while i < (len(new_polymer) - 1):
+			if new_polymer[i] ==  new_polymer[i + 1].swapcase():
+				matches = True
+				new_polymer = new_polymer.replace(new_polymer[i] + new_polymer[i + 1], '', 1)
+				#polymer = new_polymer
+			else:	
+				i = i + 1
+
+
+
+	polymer_size.append(len(new_polymer))
+polymer_size.sort()
+#correct Answer for part 2
+print(polymer_size[0]) 
+
+
+
